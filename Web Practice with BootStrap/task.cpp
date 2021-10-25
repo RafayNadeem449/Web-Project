a3d9997823abe4d100206bc69d78a65921a5753d
Rafay Nadeem <rafaynadeem29@gmail.com>
2021-10-25 19:32:53 +0500

    first commit

---
+++ Web Practice with BootStrap/task.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;  /* data */
+
+class SearchEngine
+{
+    private:
+    int size;
+    int* arr;
+
+    public:
+
+    SearchEngine(int)
+    {
+        size=0;
+    }
+
+    void Search()
+    {
+        for(int i=0;i<=5;i++)
+        {
+            if(arr[i]=-1)
+            {
+                
+            }
+        }
+    }
+
+
+
+
+};
